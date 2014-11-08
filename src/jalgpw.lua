955891404e4f7bb729a6b9614e335f44f5b17132
Argentumbolo <argentumbolo@yandex.ua>
2014-11-08 22:26:38 +0200

    First stub Plot function. Add ignoring Kate tmp files.

--- src/jalgpw.lua
+++ src/jalgpw.lua
@@ -1,8 +1,23 @@
+-- Create plot object.
+local function Plot()
+	local P = {
+		lines = {}
+	}
+	
+	local P_mt = {
+		;
+	}
+end
+
 -- Create main gnuplot object.
 local function GnuPlot()
 	local GP = {
 		is_multiplot = false,
-		plots = {}
+		plots = {},
+		
+		addPlot = function(self, plot)
+			;
+		end
 	}
 	
 	local GP_mt = {
